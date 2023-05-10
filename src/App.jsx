2cb572f3e812c021677b06ff1ec0edb065bd4816
Ausra <munduswebstudio@gmail.com>
2023-05-10 16:41:11 +0300

    React State Homework Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.scss';
 import {LabasZuiki, Tekstas, Dryziai, Planetos, Namai} from "./Components/nd-react-base";
 import {SuoKvadrate, SuoApskritime, SuoFormose, DogBigLetter, DogsColor} from "./Components/nd-react-base-list";
+import Button from './Components/nd-react-state';
 import Dogs from './Components/pvz-dogs';
+
 
 function App() {
   
   return (
     <div className="App">
       <header className="App-header">
+        
+        <Button />
         
         <LabasZuiki />
         <Tekstas text= 'Ką veiki?'/>
